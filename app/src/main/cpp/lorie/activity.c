5646254bced6451660949a70781bacf05020c6a5
Twaik Yont <9674930+twaik@users.noreply.github.com>
2026-05-31 16:22:29 +0300

    fix(renderer): defer rendererInit from JNI_OnLoad to nativeInit
    rendererInit() called FindClass("LorieView") during JNI_OnLoad, triggering
    LorieView's static initializer while libXlorie.so was still loading. In
    app_process mode this caused System.loadLibrary("Xlorie") to fail since
    nativeLibraryDirectories doesn't include the APK's lib path.
    
    Signed-off-by: Twaik Yont <9674930+twaik@users.noreply.github.com>
    (cherry picked from commit f9611e9a879adb7e5df6f92a56f1110e16160ea6)

--- app/src/main/cpp/lorie/activity.c
+++ app/src/main/cpp/lorie/activity.c
@@ -124,6 +124,8 @@ static void nativeInit(JNIEnv *env, jobject thiz) {
         MainActivity.resetIme = FindMethodOrDie(env, (*env)->GetObjectClass(env, thiz), "resetIme", "()V", JNI_FALSE);
     }
 
+    rendererInit(env);
+
     (*env)->GetJavaVM(env, &vm);
     (*vm)->AttachCurrentThread(vm, &guienv, NULL);
     globalThiz = (*guienv)->NewGlobalRef(env, thiz);
@@ -415,8 +417,6 @@ JNIEXPORT jint JNI_OnLoad(JavaVM *vm, __unused void *reserved) {
     (*vm)->AttachCurrentThread(vm, &env, NULL);
     jclass cls = (*env)->FindClass(env, "com/termux/x11/LorieView");
     (*env)->RegisterNatives(env, cls, methods, sizeof(methods)/sizeof(methods[0]));
-
-    rendererInit(env);
 
     return JNI_VERSION_1_6;
 }
